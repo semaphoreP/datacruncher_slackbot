ef148f96a37af7fbef9c97c51b5f76646fc1c3ce
Jason Wang <jason.wang@outlook.com>
2016-04-17 23:18:03 -0700

    Bot's first message! Beep boop. README formatting update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-===== The GPIES Data Cruncher Slack Bot =====
+# The GPIES Data Cruncher Slack Bot
 
 This is an automated bot to allow the GPIES Data Cruncher chat on Slack.
 
-=== Requirements ===
+### Requirements
   * Python3 (configparaser)
   * slackclient python library
--- bot.py
+++ bot.py
@@ -7,3 +7,9 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 username = config['DEFAULT']['username']
 token = config['DEFAULT']['token']
+
+
+client = SlackClient(token)
+print(client.api_call(
+    "chat.postMessage", channel="#gpies-observing", text="Beep. Boop.",
+    username=username, as_user=True))
