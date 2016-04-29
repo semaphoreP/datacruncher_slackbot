feb0bf3fc33b830315733d82c2e30cd053a2e387
Marshall Perrin <mperrin@stsci.edu>
2016-04-29 18:44:48 -0400

    improve sunrise/set functionality

--- suntimes.py
+++ suntimes.py
@@ -2,6 +2,7 @@ import ephem
 import pytz
 import copy
 from datetime import datetime, timedelta
+import numpy as np
 
 import timezone
 
@@ -28,11 +29,18 @@ def _gemini():
     gemini.elevation=2722
     gemini.temp=0
     gemini.pressure=726
+    gemini.horizon = '-1:45'  # earth's horizon is below local horizontal if you're on a mountain.
     return gemini
 
 gemsouth = _gemini()
 gemsouth_twi = _gemini()
 gemsouth_twi.horizon='-12'
+
+def delta_to_now(sometime):
+    deltat = (sometime - ephem.now()) # in days
+    delta_hour = int(np.floor(deltat*24))
+    delta_min = int(np.round((deltat-delta_hour/24)*24*60))
+    return delta_hour,delta_min
 
 
 def format_time(dt, tz):
@@ -52,16 +60,18 @@ def sunrise_time_response():
 
     risetime = gemsouth.next_rising(ephem.Sun())
     twitime = gemsouth_twi.next_rising(ephem.Sun(), use_center=True)
-    return ("Next sunrise at Gemini South is {}".format(utc_to_multizone(risetime.datetime())) +
-            "\nand 12 deg twilight is at {}".format(utc_to_multizone(twitime.datetime()) ) )
+    return ("Next sunrise at Gemini South is {}, which is {} h {} m from now.".format(utc_to_multizone(risetime.datetime()), *delta_to_now(risetime)) +
+            "\nAnd 12 deg twilight is at {}".format(utc_to_multizone(twitime.datetime()) ) )
+
 
 
 def sunset_time_response():
     gemsouth.date = ephem.now()
     gemsouth_twi.date = ephem.now()
     settime = gemsouth.next_setting(ephem.Sun())
+
     twitime = gemsouth_twi.next_setting(ephem.Sun(), use_center=True)
-    return ("Next sunset at Gemini South is {}".format(utc_to_multizone(settime.datetime())) +
-            "\nand 12 deg twilight is at {}".format(utc_to_multizone(twitime.datetime()) ) )
+    return ("Next sunset at Gemini South is {}, which is {} h {} m from now.".format(utc_to_multizone(settime.datetime()), *delta_to_now(settime)) +
+            "\nAnd 12 deg twilight is at {}".format(utc_to_multizone(twitime.datetime()) ) )
 
 
